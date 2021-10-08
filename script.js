81dfe62b48406617e8f212ffa7cb40bb3debd4be
kamilleSvalgaard <kami7277@stud.kea.dk>
2021-10-08 14:44:36 +0200

    første gsap animationer

--- script.js
+++ script.js
@@ -1,15 +1,90 @@
-// gsap.from(".logo", {
-//   x: 300,
-//   scale: 0.3,
-//   rotation: 360,
-//   duration: 2,
-//   ease: "bounce.out",
-//   scrollTrigger: {
-//     trigger: ".logo",
-//     // markers: true,
-//     start: "center center",
-//     end: "center 25%",
-//     scrub: true,
-//     pin: true,
-//   },
-// });
+gsap.from(".logo", {
+  scale: 1.3,
+  duration: 0.5,
+  scrollTrigger: {
+    trigger: ".logo",
+    // markers: true,
+    toggleActions: "play none none reverse",
+    start: "center center",
+    end: "center 25%",
+    scrub: true,
+  },
+});
+
+gsap.from(".nav-link", {
+  x: -500,
+  duration: 1,
+  stagger: -0.3,
+});
+
+gsap.from(".section2gsap", {
+  x: 500,
+  duration: 1,
+  scrollTrigger: {
+    trigger: ".nedad",
+    // markers: true,
+    toggleActions: "play none none reverse",
+    start: "center center",
+    end: "center 25%",
+  },
+});
+
+gsap.from(".first", {
+  x: -500,
+  duration: 2,
+  scrollTrigger: {
+    trigger: ".section2gsap",
+    // markers: true,
+    toggleActions: "play none none reverse",
+    start: "center center",
+    end: "center 25%",
+  },
+});
+
+gsap.from(".second", {
+  x: 500,
+  duration: 3,
+  scrollTrigger: {
+    trigger: ".section2gsap",
+    // markers: true,
+    toggleActions: "play none none reverse",
+    start: "center center",
+    end: "center 25%",
+  },
+});
+
+gsap.from(".section3gsap", {
+  x: -500,
+  duration: 2,
+  scrollTrigger: {
+    trigger: ".first",
+    // markers: true,
+    toggleActions: "play none none reverse",
+    start: "center center",
+    end: "center 25%",
+  },
+});
+
+gsap.from(".section3label", {
+  x: 500,
+  duration: 3,
+  scrollTrigger: {
+    trigger: ".second",
+    // markers: true,
+    toggleActions: "play none none reverse",
+    start: "center center",
+    end: "center 25%",
+  },
+});
+
+gsap.from(".info1", {
+  scale: 0.7,
+  duration: 1,
+  scrollTrigger: {
+    trigger: ".section3label",
+    // markers: true,
+    toggleActions: "play none none reverse",
+    start: "bottom center",
+    end: "bottom 25%",
+  },
+});
