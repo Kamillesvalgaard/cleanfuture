2eed8a059166659bba07eb82d87b8ae374b531f5
kamilleSvalgaard <kami7277@stud.kea.dk>
2021-10-08 12:26:26 +0200

    animation test js gsap

---
+++ script.js
@@ -0,0 +1,15 @@
+// gsap.from(".logo", {
+//   x: 300,
+//   scale: 0.3,
+//   rotation: 360,
+//   duration: 2,
+//   ease: "bounce.out",
+//   scrollTrigger: {
+//     trigger: ".logo",
+//     // markers: true,
+//     start: "center center",
+//     end: "center 25%",
+//     scrub: true,
+//     pin: true,
+//   },
+// });
