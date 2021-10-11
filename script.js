f92374a1fa3f2e2dfc6e4c479135ac59d7a3fa8d
kamilleSvalgaard <kami7277@stud.kea.dk>
2021-10-11 10:50:41 +0100

    animationer mobil

--- script.js
+++ script.js
@@ -15,11 +15,13 @@ gsap.from(".nav-link", {
   x: -500,
   duration: 1,
   stagger: -0.3,
+  opacity: 0,
 });
 
 gsap.from(".section2gsap", {
   x: 500,
   duration: 1,
+  opacity: 0,
   scrollTrigger: {
     trigger: ".nedad",
     // markers: true,
@@ -80,11 +82,90 @@ gsap.from(".section3label", {
 gsap.from(".info1", {
   scale: 0.7,
   duration: 1,
+  opacity: 0,
   scrollTrigger: {
     trigger: ".section3label",
     // markers: true,
     toggleActions: "play none none reverse",
     start: "bottom center",
     end: "bottom 25%",
+  },
+});
+
+gsap.from(".container", {
+  x: -500,
+  duration: 1,
+  opacity: 0,
+  scrollTrigger: {
+    trigger: ".info1",
+    // markers: true,
+    toggleActions: "play none none reverse",
+    start: "center center",
+    end: "center 25%",
+  },
+});
+
+gsap.from(".container1", {
+  x: -500,
+  duration: 1,
+  opacity: 0,
+  scrollTrigger: {
+    trigger: ".info1",
+    // markers: true,
+    toggleActions: "play none none reverse",
+    start: "center center",
+    end: "center 25%",
+  },
+});
+
+gsap.from(".row", {
+  x: 1000,
+  duration: 1,
+  opacity: 0,
+  scrollTrigger: {
+    trigger: ".row",
+    // markers: true,
+    toggleActions: "play none none reverse",
+    start: "center center",
+    end: "center 25%",
+  },
+});
+
+gsap.from("#section6", {
+  scale: 0.7,
+  duration: 1,
+  opacity: 0,
+  scrollTrigger: {
+    trigger: "#section6",
+    // markers: true,
+    toggleActions: "play none none reverse",
+    start: "bottom bottom",
+    end: "top 25%",
+  },
+});
+
+gsap.from("#section7", {
+  x: 500,
+  duration: 1,
+  opacity: 0,
+  scrollTrigger: {
+    trigger: ".opdelertrigger2",
+    // markers: true,
+    toggleActions: "play none none reverse",
+    start: "center center",
+    end: "center 25%",
+  },
+});
+
+gsap.from(".container2", {
+  x: -1000,
+  duration: 1,
+  opacity: 0,
+  scrollTrigger: {
+    trigger: ".opdelertrigger2",
+    // markers: true,
+    toggleActions: "play none none reverse",
+    start: "center center",
+    end: "center 25%",
   },
 });
